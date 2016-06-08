9f4585dd51c3a9d28a213f9aaa96509f4b4c4e27
RobAdmin <RobAdmin>
2016-06-08 18:08:38 +0200

    changes from regus office

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$9f851b22ddcc6b65$e43a971e667c9b83c9e18018e7925aa6b05279fe"
+password="pbkdf2(1000,20,sha512)$9aadf7202a1cd25c$5c9c7fe4c8fefe57d8a3e6e31a79645dae01086f"
